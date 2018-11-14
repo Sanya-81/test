8500e5b6b61881d68923e03cb17f85feec8facda
Aleksandr Tomashov <sanya-81-shess@mail.ru>
2018-11-14 20:49:08 +0800

    apgrede js file: phone-page.js, search, sorter, phone-service

--- js/scripts/phones/components/services/phones-service.js
+++ js/scripts/phones/components/services/phones-service.js
@@ -1,13 +1,38 @@
 'use strict';
 
 const phonesService = {
-    loadPhones(callback) {
-        this._sendRequest('/api/phones', callback);
+    loadPhones(filter, callback) {
+        this._sendRequest('/api/phones', (phones) => {
+            const filterdPhones = this._filter(phones, filter.query);
+            const sortedPhones = this._sort(filterdPhones, filter.order);
+        
+            callback(sortedPhones);
+        });
     },  
 
     loadPhone(phoneId, callback) {
         this._sendRequest(`/api/phones/${phoneId}`, callback);
     }, 
+
+    _filter(phohes, query) {
+        if (!query) {
+            return phohes;
+        }
+
+        let normaLizedQuery = query.toLowerCase();
+
+        return phohes.filter((phone) =>{
+            return phone.name.toLowerCase().include(normaLizedQuery);
+        });
+    },
+
+    _sort(phones, orderField) {
+        return phones.sort((phoneA, phoneB) =>{
+            return (phoneA[orderField] > phoneB[orderField])
+                ? 1
+                : -1;
+        });
+    },
 
     _sendRequest( url, callback, { method = 'GET'} = {}) {
         let xhr = new XMLHttpRequest();
--- js/scripts/phones/phones-page.js
+++ js/scripts/phones/phones-page.js
@@ -10,12 +10,34 @@ export default class PhonesPage {
   
   constructor({element}){
     this._element = element;
-    
+
+    this._filter = {
+      query: '',
+      order: 'name',
+    };
+
+    this._initCatalog();
+    this._initViewer();
+    this._shoppingCart();
+    this._initFilter();
+
+    this._refreshPhones();
+  }
+
+  _refreshPhones() {
+    const callback = (phones) => {
+      this._catalog.setPhones(phones);    
+    }; 
+
+    phonesService.loadPhones(this._filter, callback);
+  }
+
+  _initCatalog() {
     this._catalog = new PhonesCatalog({
-      element: this._element.querySelector('[data-component="phones-catalog"]')
+      element: this._element.querySelector('[data-component="phones-catalog"]') 
     });
 
-    phonesService.loadPhones((phones) => {
+    phonesService.loadPhones((phones) => {   
       this._catalog.setPhones(phones);
     });
 
@@ -33,7 +55,9 @@ export default class PhonesPage {
 
         this._shoppingCart.addItem(phoneId);
     });
-    
+  }
+
+  _initViewer() {
     this._viewer = new PhoneViewer({
       element: this._element.querySelector('[data-component="phone-viewer"]'),
     });
@@ -42,18 +66,31 @@ export default class PhonesPage {
         this._viewer.hide();
         this._catalog.show();
     });
+  }
 
-    this._sorter = new Sorter({
+  _shoppingCart() {
+    this._shoppingCart = new ShoppingCart({
+      element: this._element.querySelector('[data-component="shopping-cart"]'),
+    });
+  }
+
+  _initFilter() {
+          this._sorter = new Sorter({
       element: this._element.querySelector('[data-component="sorter"]'),
     });
 
     this._search = new Search({
       element: this._element.querySelector('[data-component="search"]'),
     });
-
-
-    this._shoppingCart = new ShoppingCart({
-      element: this._element.querySelector('[data-component="shopping-cart"]'),
+ 
+    this._search.on('search', (event) => {
+      this._filter.query = event.detail;
+      this._refreshPhones();
+    }); 
+  
+    this._sorter.on('changeOrder', (event) => {
+      this._filter.order = event.detail;
+      this._refreshPhones();
     });
-  } 
+  }
 } 
